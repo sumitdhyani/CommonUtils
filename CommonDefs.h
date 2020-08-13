8695d007117025452894b80ecdcb86b381b32ede
U-IONTRADING\Sumit.Dhyani <Sumit.Dhyani@IONDELLT259.iontrading.com>
2020-08-13 13:10:13 +0530

    Corrected some spellings

--- CommonDefs.h
+++ CommonDefs.h
@@ -8,15 +8,15 @@
 #define define_ptr(type) typedef std::shared_ptr<type> type##_sptr;
 #define define_unique_ptr(type) typedef std::unique_ptr<type> type##_uptr;
 
-typedef std::mutex stdmutex;
-typedef std::condition_variable stdconnditionvariable;
-typedef std::unique_lock<std::mutex> stduniquelock;
+typedef std::mutex stdMutex;
+typedef std::condition_variable stdConnditionvariable;
+typedef std::unique_lock<std::mutex> stdUniquelock;
 typedef std::thread stdthread;
 typedef std::chrono::system_clock::time_point time_point;
 typedef std::chrono::system_clock::duration duration;
 
-define_ptr(stdmutex)
-define_ptr(stdconnditionvariable)
+define_ptr(stdMutex)
+define_ptr(stdConnditionvariable)
 typedef unsigned int uint;
 
 
