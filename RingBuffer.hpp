8766f28e6f5a62fccf13f6b2756b071596ff6b42
Sumit Dhyani <sumit.dhyani@iongroup.com>
2021-02-15 16:02:13 +0530

    Simplified the implementation of ringbuffer

--- RingBuffer.hpp
+++ RingBuffer.hpp
@@ -1,55 +1,39 @@
 #pragma once
-#include "CommonDefs.hpp"
-#include <vector>
+#include "CommonUtils/CommonDefs.hpp"
+#include <queue>
 
 namespace ULCommonUtils
 {
 	template <class T>
 	class RingBuffer
 	{
-		std::vector<T> m_queue;
+		std::queue<T> m_queue;
 		size_t m_capacity;
-		size_t m_start;
-		size_t m_end;
-
-		bool m_firstElementPushed;
 	public:
 		RingBuffer(size_t capacity) :
-			m_capacity(capacity),
-			m_queue(capacity, T())
+			m_capacity(capacity)
 		{
-			m_start = 1;
-			m_end = 0;
-			m_firstElementPushed = false;
 		}
 
 		void push(T item)
 		{
-			m_firstElementPushed = true;
-			m_end = (m_end + 1) % m_capacity;
-			m_queue[m_end] = item;
-			if (m_end == m_start)
-				m_start = (m_start + 1) % m_capacity;
+			if (full())
+				pop();
+
+			m_queue.push(item);
 		}
 
-		T pop()
+		void pop()
 		{
 			if (empty())
 				throw std::runtime_error("Trying to pop empty queue");
 
-			T retVal = m_queue[m_start];
-			m_start = (m_start + 1) % m_capacity;
-			return retVal;
+			m_queue.pop();
 		}
 
 		size_t size()
 		{
-			if (!m_firstElementPushed)
-				return 0;
-			else
-				return (m_end >= m_start) ?
-				(m_end - m_start + 1) :
-				(m_capacity - (m_start - m_end - 1));
+			return m_queue.size();
 		}
 
 		size_t empty()
@@ -67,7 +51,7 @@ namespace ULCommonUtils
 			if (empty())
 				throw std::runtime_error("Trying to access element when buffer is empty");
 
-			return m_queue[m_start];
+			return m_queue.front();
 		}
 
 		T back()
@@ -75,7 +59,7 @@ namespace ULCommonUtils
 			if (empty())
 				throw std::runtime_error("Trying to access element when buffer is empty");
 
-			return m_queue[m_end];
+			return m_queue.back();
 		}
 	};
 }
